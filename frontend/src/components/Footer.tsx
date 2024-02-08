d0d0bc29011302796b543c01be8a43011c5297dc
Anna Daszyńska <anna.daszynska@protonmail.com>
2024-02-08 12:09:12 +0100

    changed the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,17 +6,7 @@ const Footer = () => {
           <span className="text-3xl text-white font-bold">
             HolidayRooms.com
           </span>
-          <span className="text-white font-bold">
-            <p>
-              © 2024 Anna Daszyńska - robiłam tę stronę z dużą dozą ciepła (i
-              czekolady).
-            </p>
-          </span>
         </div>
-        <span className="text-white font-bold tracking-tight flex gap-4">
-          <p className="cursor-pointer">Polityka prywatności</p>
-          <p className="cursor-pointer">Warunki użytkowania </p>
-        </span>
       </div>
     </div>
   );
